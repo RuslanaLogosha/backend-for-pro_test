00e4a2c789dca19f6d040fe0c564ecba91ae4e11
Aleksandr Mukomelov <mukomelov.aleksandr@gmail.com>
2021-04-13 14:00:46 +0300

    feat: test results endpoint now also send data about percentage of correct answers and wordings, that describe acvieved result

--- controllers/quiz-tech-controller.js
+++ controllers/quiz-tech-controller.js
@@ -1,36 +1,28 @@
 const TechQuiz = require('../model/quiz-tech-model');
 const { HttpCode, Status } = require('../helpers/constants');
 const { getRandomInt } = require('../helpers/get-random-integer');
+const resultWordings = require('../helpers/test-result-reply.json');
 
 async function getRandomTechQuestions(req, res, next) {
   try {
-    // const userId = req.user.id;
-
     // fetching all questions from db
     const allQuestions = await TechQuiz.getAllQuestions();
-
     // forming a list of 12 random questions
     const randomQuestions = [];
-
     do {
       const randomIndex = getRandomInt(0, 25);
-      // console.log(randomIndex);
       const questionToInclude = allQuestions[randomIndex];
-
       if (!randomQuestions.includes(questionToInclude)) {
         randomQuestions.push(questionToInclude);
       }
     } while (randomQuestions.length < 12);
-
-    // console.log(randomQuestions.length);
-
     // mapping resulted list to return only specific field
     const mappedRandomQuestions = randomQuestions.map(el => ({
       questionId: el.questionId,
       question: el.question,
       answers: el.answers,
     }));
-
+    // sending successful response
     return res.status(HttpCode.OK).json({
       status: Status.SUCCESS,
       code: HttpCode.OK,
@@ -45,7 +37,6 @@ async function getTechResults(req, res, next) {
   try {
     // accepting results as send by front-end
     const resultsToCheck = req.body;
-
     // checking results
     const checkedResults = await Promise.all(
       resultsToCheck.map(async el => {
@@ -57,17 +48,29 @@ async function getTechResults(req, res, next) {
         };
       }),
     );
-
     // counting correct answers
     const correctAnswersCount = checkedResults.filter(el => el.result === true);
-
+    // counting correct / incorrect percentage
+    const correctPercentage = Number(
+      Math.round((correctAnswersCount.length / checkedResults.length) * 100),
+    );
+    const incorrectPercentage = Number(100 - correctPercentage);
+    // getting wordings
+    const { aboutResultTitle, aboutResultSubtitle } = resultWordings.find(
+      ({ id }) => id === String(correctAnswersCount.length),
+    );
+    // sending successful response
     return res.status(HttpCode.OK).json({
       status: Status.SUCCESS,
       code: HttpCode.OK,
       data: {
         testType: 'Tech',
-        totalAnswersCount: String(checkedResults.length),
-        correctAnswersCount: String(correctAnswersCount.length),
+        totalAnswersCount: Number(checkedResults.length),
+        correctAnswersCount: Number(correctAnswersCount.length),
+        correctPercentage,
+        incorrectPercentage,
+        aboutResultTitle,
+        aboutResultSubtitle,
       },
     });
   } catch (e) {
--- controllers/quiz-theory-controller.js
+++ controllers/quiz-theory-controller.js
@@ -1,17 +1,14 @@
 const TheoryQuiz = require('../model/quiz-theory-model');
 const { HttpCode, Status } = require('../helpers/constants');
 const { getRandomInt } = require('../helpers/get-random-integer');
+const resultWordings = require('../helpers/test-result-reply.json');
 
 async function getRandomTheoryQuestions(req, res, next) {
   try {
-    // const userId = req.user.id;
-
     // fetching all questions from db
     const allQuestions = await TheoryQuiz.getAllQuestions();
-
     // forming a list of 12 random questions
     const randomQuestions = [];
-
     do {
       const randomIndex = getRandomInt(0, 30);
       // console.log(randomIndex);
@@ -21,16 +18,13 @@ async function getRandomTheoryQuestions(req, res, next) {
         randomQuestions.push(questionToInclude);
       }
     } while (randomQuestions.length < 12);
-
-    // console.log(randomQuestions.length);
-
     // mapping resulted list to return only specific field
     const mappedRandomQuestions = randomQuestions.map(el => ({
       questionId: el.questionId,
       question: el.question,
       answers: el.answers,
     }));
-
+    // sending successful response
     return res.status(HttpCode.OK).json({
       status: Status.SUCCESS,
       code: HttpCode.OK,
@@ -45,7 +39,6 @@ async function getTheoryResults(req, res, next) {
   try {
     // accepting results as send by front-end
     const resultsToCheck = req.body;
-
     // checking results
     const checkedResults = await Promise.all(
       resultsToCheck.map(async el => {
@@ -57,17 +50,29 @@ async function getTheoryResults(req, res, next) {
         };
       }),
     );
-
     // counting correct answers
     const correctAnswersCount = checkedResults.filter(el => el.result === true);
-
+    // counting correct / incorrect percentage
+    const correctPercentage = Number(
+      Math.round((correctAnswersCount.length / checkedResults.length) * 100),
+    );
+    const incorrectPercentage = Number(100 - correctPercentage);
+    // getting wordings
+    const { aboutResultTitle, aboutResultSubtitle } = resultWordings.find(
+      ({ id }) => id === String(correctAnswersCount.length),
+    );
+    // sending successful response
     return res.status(HttpCode.OK).json({
       status: Status.SUCCESS,
       code: HttpCode.OK,
       data: {
         testType: 'Theory',
-        totalAnswersCount: String(checkedResults.length),
-        correctAnswersCount: String(correctAnswersCount.length),
+        totalAnswersCount: Number(checkedResults.length),
+        correctAnswersCount: Number(correctAnswersCount.length),
+        correctPercentage,
+        incorrectPercentage,
+        aboutResultTitle,
+        aboutResultSubtitle,
       },
     });
   } catch (e) {
