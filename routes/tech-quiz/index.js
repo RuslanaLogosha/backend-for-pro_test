50a8ed361d9d63bd3ef31203a2e0da2beb0b3ece
Aleksandr Mukomelov <mukomelov.aleksandr@gmail.com>
2021-04-14 20:24:20 +0300

    feat: test endpoints now are available only to logged users

--- routes/tech-quiz/index.js
+++ routes/tech-quiz/index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const quizTechController = require('../../controllers/quiz-tech-controller');
-// const guard = require('../../helpers/guard');
+const guard = require('../../helpers/guard');
 
-router.get('/questions', quizTechController.getRandomTechQuestions);
-router.post('/results', quizTechController.getTechResults);
+router.get('/questions', guard, quizTechController.getRandomTechQuestions);
+router.post('/results', guard, quizTechController.getTechResults);
 
 module.exports = router;
--- routes/theory-quiz/index.js
+++ routes/theory-quiz/index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const quizTheoryController = require('../../controllers/quiz-theory-controller');
-// const guard = require('../../helpers/guard');
+const guard = require('../../helpers/guard');
 
-router.get('/questions', quizTheoryController.getRandomTheoryQuestions);
-router.post('/results', quizTheoryController.getTheoryResults);
+router.get('/questions', guard, quizTheoryController.getRandomTheoryQuestions);
+router.post('/results', guard, quizTheoryController.getTheoryResults);
 
 module.exports = router;
